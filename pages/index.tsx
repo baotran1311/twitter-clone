ddeae300c7722e9277a1b286c2a96bedf5e3263e
baotran1311 <baotran13112003@gmail.com>
2024-06-02 23:28:45 +0700

    auth ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,8 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-   
-     <>
-     <Header label="home" />
-     </>
-    
+    <>
+      <Header label="home" />
+    </>
   );
 }
